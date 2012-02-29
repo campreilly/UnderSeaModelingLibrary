1503ad391e95c4b94e45d4990450a7bd02dd5d54
Sean Reilly <sreilly@aegistg.com>
2012-02-29 16:17:34 -0500

    Consistently use underlines in file names.

--- make_tar.sh
+++ make_tar.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 version=0.02
-tar --exclude=data --exclude=doc --exclude=html --exclude= usml-api.html \
+tar --exclude=data --exclude=doc --exclude=html --exclude=usml_api.html \
     -czf ../usml-src-$version.tar.gz *
-tar -czf ../usml-doc-$version.tar.gz usml-api.html html/*
+tar -czf ../usml-doc-$version.tar.gz usml_api.html html/*
 zip --quiet -r ../usml-src-$version.zip * \
-    -x "data/*" -x "doc/*" -x "html/*" -x usml-api.html
-zip --quiet ../usml-doc-$version.zip usml-api.html html/*
+    -x "data/*" -x "doc/*" -x "html/*" -x usml_api.html
+zip --quiet ../usml-doc-$version.zip usml_api.html html/*
+cp usml_install.html ..
