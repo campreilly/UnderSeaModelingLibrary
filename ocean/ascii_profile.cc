9fe31945d172d8ecc2717dca7cfd46075d6f3be1
Sean Reilly <sreilly@aegistg.com>
2012-05-04 08:22:54 -0400

    Add ascii_profile class to read SVP from text file.

---
+++ ocean/ascii_profile.cc
@@ -0,0 +1,53 @@
+/** 
+ * @file ascii_profile.cc
+ * Read a 1-D profile from a text file.
+ */
+ 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctype.h>
+#include <usml/ocean/ascii_profile.h>
+
+using namespace usml::ocean ;
+
+/**
+ * Read a 1-D profile from a text file.
+ */
+ascii_profile::ascii_profile( const char* filename ) {
+
+    // count the number of lines in the input file
+
+    std::ifstream infile(filename);
+    unsigned size = 0 ;
+    std::string line ;
+    while ( getline(infile,line) ) ++size ;
+    infile.clear() ;
+    infile.seekg(0) ;
+    cout << "filename=" << filename << " size=" << size << endl ;
+
+    // read data from input file
+
+    double depth ;
+    double *height = new double[size] ;
+    double *speed = new double[size] ;
+    char c ;
+    for ( unsigned n=0 ; n < size ; ++n ) {
+        infile >> depth ;
+        while ( true ) {    // skip comma and space delimiters
+            c = infile.peek() ;
+            if ( c != ',' && ! isspace(c) ) break ;
+            infile.get() ;
+        }
+        infile >> speed[n] ;
+        height[n] = wposition::earth_radius - depth ;
+        cout << depth << "\t" << speed[n] << endl ;
+    }
+
+    // load into data_grid variables
+
+    this->_axis[0] = new seq_data( height, size ) ;
+    this->_data = speed ;
+    delete[] height ;
+}
+
---
+++ ocean/ascii_profile.h
@@ -0,0 +1,39 @@
+/** 
+ * @file ascii_profile.h
+ * Read a 1-D profile from a text file.
+ */
+#ifndef USML_ASCII_PROFILE_H
+#define USML_ASCII_PROFILE_H
+
+#include <usml/ocean/ocean.h>
+
+namespace usml {
+namespace ocean {
+
+/// @ingroup profiles
+/// @{
+
+/**
+ * Read a 1-D profile from a text file.  The is often used to read
+ * tables and CSV files from other applications.
+ */
+class USML_DECLSPEC ascii_profile : public data_grid<double,1> {
+
+  public:
+
+    //**************************************************
+    // initialization
+
+    /**
+     * Read a 1-D profile from a file.
+     *
+     * @param filename  File to be named.
+     */
+    ascii_profile( const char* filename ) ;
+
+};
+
+}  // end of namespace ocean
+}  // end of namespace usml
+
+#endif
