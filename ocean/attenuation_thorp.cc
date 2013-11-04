d66d2ed2e7bd8e1ca18d3c9c966c376e2a234b4c
islanderjtn <tburns@aegistg.com>
2013-11-04 09:45:03 -0500

    Resoloved Multithreading on Attenuation Thorp.  Removed Mutlithreading from Attenuation Throp and Profile_MT

--- ocean/attenuation_thorp.cc
+++ ocean/attenuation_thorp.cc
@@ -15,33 +15,23 @@ void attenuation_thorp::attenuation(
         const seq_vector& frequencies,
         const matrix<double>& distance,
         matrix< vector<double> >* attenuation) {
-    // initialize the cache for the attenuation coefficients
-    // reinitialize if frequency values have changed
 
-    if (prev_freq != &frequencies) {
-        att_coeff.resize( location.size1(), location.size2() ) ;
-        for (unsigned row = 0; row < location.size1(); ++row) {
-            for (unsigned col = 0; col < location.size2(); ++col) {
-                att_coeff(row, col).resize(frequencies.size());
-                for (unsigned f = 0; f < frequencies.size(); ++f) {
-                    double F2 = frequencies(f);
-                    F2 = 1e-6 * F2 * F2;
-                    att_coeff(row, col)(f) = 1e-3 *
-                        (3.3e-3 + F2 * (0.11 / (1.0 + F2)
-                        + 44.0 / (4100.0 + F2) + 3.0e-4))
-                        / (1.0 - 5.88264e-6 * 1000.0);
-                }
-            }
-        }
-        prev_freq = &frequencies;
+	// initialize the cache for the attenuation coefficients
+    vector <double> alpha(frequencies.size());
+    for (unsigned f = 0; f < frequencies.size(); ++f) {
+		double F2 = frequencies(f);
+		F2 = 1e-6 * F2 * F2;
+		alpha(f) = 1e-3 *
+			(3.3e-3 + F2 * (0.11 / (1.0 + F2)
+			+ 44.0 / (4100.0 + F2) + 3.0e-4))
+			/ (1.0 - 5.88264e-6 * 1000.0);
     }
-
     // apply attenuation coefficients and depth corrections
-
     for (unsigned row = 0; row < location.size1(); ++row) {
         for (unsigned col = 0; col < location.size2(); ++col) {
             (*attenuation)(row, col) =
-                distance(row, col) * att_coeff(row, col)
+                distance(row, col)
+                * alpha
                 * (1.0 + 5.88264e-6 * location.altitude(row, col)) ;
         }
     }
