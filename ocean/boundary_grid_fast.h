e953633fca07afe470eb14a313ba1849941e7aa6
islanderjtn <tburns@aegistg.com>
2014-01-07 11:21:50 -0500

    Update 1.7.2014 11:20 New Files Reformatted for Style Compliance.

--- ocean/boundary_grid_fast.h
+++ ocean/boundary_grid_fast.h
@@ -26,17 +26,43 @@ namespace ocean {
  * latitude/longitude axes defined by the data grid at limited to the values
  * at the grid edge.
  */
-class boundary_grid_fast : public boundary_model
-{
+class boundary_grid_fast : public boundary_model {
+
+public:
+
     //**************************************************
     // height model
 
-protected:
+    /**
+     * Constructor - Initialize depth and reflection loss components for a boundary.
+     *
+     * @param height            Bottom depth (meters) as a function of position.
+     *                          Assumes control of this grid and deletes
+     *                          it when the class is destroyed.
+     * @param reflect_loss      Reflection loss model.  Defaults to a
+     *                          Rayleigh reflection for "sand" if NULL.
+     *                          The boundary_model takes over ownship of this
+     *                          reference and deletes it as part of its destructor.
+     */
+    boundary_grid_fast(data_grid_bathy* height,
+            reflect_loss_model* reflect_loss = NULL) :
+            boundary_model(reflect_loss), _height(height) {
+        this->_height->interp_type(0, GRID_INTERP_PCHIP);
+        this->_height->interp_type(1, GRID_INTERP_PCHIP);
+        this->_height->edge_limit(0, true);
+        this->_height->edge_limit(1, true);
+        if (_reflect_loss_model == NULL) {
+            _reflect_loss_model = new reflect_loss_rayleigh(
+                    reflect_loss_rayleigh::SAND);
+        }
+    }
 
-    /** Boundary for all locations. */
-    data_grid_bathy* _height;
-
-public:
+    /**
+     * Destructor - Delete boundary grid.
+     */
+    virtual ~boundary_grid_fast() {
+        delete _height;
+    }
 
     /**
      * Compute the height of the boundary and it's surface normal at
@@ -48,34 +74,32 @@ public:
      * @param quick_interp  Determines if you want a fast nearest or pchip interp
      */
     virtual void height(const wposition& location, matrix<double>* rho,
-        wvector* normal = NULL, bool quick_interp = false) {
-            if(quick_interp) {
-                this->_height->interp_type(0, GRID_INTERP_LINEAR);
-                this->_height->interp_type(1, GRID_INTERP_LINEAR);
-            } else{
-                this->_height->interp_type(0, GRID_INTERP_PCHIP);
-                this->_height->interp_type(1, GRID_INTERP_PCHIP);
-            }
-            if (normal) {
-                matrix<double> gtheta(location.size1(), location.size2());
-                matrix<double> gphi(location.size1(), location.size2());
-                matrix<double> t(location.size1(), location.size2());
-                matrix<double> p(location.size1(), location.size2());
-                this->_height->interpolate(location.theta(), location.phi(),
-                    rho, &gtheta, &gphi);
+            wvector* normal = NULL, bool quick_interp = false) {
+        if (quick_interp) {
+            this->_height->interp_type(0, GRID_INTERP_LINEAR);
+            this->_height->interp_type(1, GRID_INTERP_LINEAR);
+        } else {
+            this->_height->interp_type(0, GRID_INTERP_PCHIP);
+            this->_height->interp_type(1, GRID_INTERP_PCHIP);
+        }
+        if (normal) {
+            matrix<double> gtheta(location.size1(), location.size2());
+            matrix<double> gphi(location.size1(), location.size2());
+            matrix<double> t(location.size1(), location.size2());
+            matrix<double> p(location.size1(), location.size2());
+            this->_height->interpolate(location.theta(), location.phi(), rho,
+                    &gtheta, &gphi);
 
-                t = element_div(gtheta, *rho);  // slope = tan(angle)
-                p = element_div(gphi, element_prod(*rho, sin(location.theta())));
-                normal->theta(                  // normal = -sin(angle)
-                    element_div( -t, sqrt(1.0+abs2(t)) ));
-                normal->phi(
-                    element_div( -p, sqrt(1.0+abs2(p)) ));
-                normal->rho(sqrt(           	// r=sqrt(1-t^2-p^2)
-                		1.0 - abs2(normal->theta()) - abs2(normal->phi()) ));
-            } else {
-                this->_height->interpolate(location.theta(), location.phi(),
-                    rho);
-            }
+            t = element_div(gtheta, *rho);  // slope = tan(angle)
+            p = element_div(gphi, element_prod(*rho, sin(location.theta())));
+            normal->theta(                  // normal = -sin(angle)
+                    element_div(-t, sqrt(1.0 + abs2(t))));
+            normal->phi(element_div(-p, sqrt(1.0 + abs2(p))));
+            normal->rho(sqrt(           	// r=sqrt(1-t^2-p^2)
+                    1.0 - abs2(normal->theta()) - abs2(normal->phi())));
+        } else {
+            this->_height->interpolate(location.theta(), location.phi(), rho);
+        }
     }
 
     /**
@@ -88,66 +112,37 @@ public:
      * @param quick_interp  Determines if you want a fast nearest or pchip interp
      */
     virtual void height(const wposition1& location, double* rho,
-        wvector1* normal = NULL, bool quick_interp = false) {
-            if(quick_interp) {
-                this->_height->interp_type(0, GRID_INTERP_LINEAR);
-                this->_height->interp_type(1, GRID_INTERP_LINEAR);
-            } else{
-                this->_height->interp_type(0, GRID_INTERP_PCHIP);
-                this->_height->interp_type(1, GRID_INTERP_PCHIP);
-            }
-            if (normal) {
-                double loc[2] = { location.theta(), location.phi() };
-                double grad[2];
-                *rho = this->_height->interpolate(loc, grad);
-                const double t = grad[0] / (*rho);      // slope = tan(angle)
-                const double p = grad[1] / ((*rho) * sin(location.theta()));
-                normal->theta(-t / sqrt(1.0 + t * t));  // normal = -sin(angle)
-                normal->phi(-p / sqrt(1.0 + p * p));
-                const double N = normal->theta() * normal->theta()
-                               + normal->phi()  * normal->phi();
-                normal->rho( sqrt(1.0-N) );				// r=sqrt(1-t^2-p^2)
-            } else {
-                double loc[2] = { location.theta(), location.phi() };
-                *rho = this->_height->interpolate(loc);
-            }
-    }
-
-    //**************************************************
-    // initialization
-
-    /**
-     * Initialize depth and reflection loss components for a boundary.
-     *
-     * @param height            Bottom depth (meters) as a function of position.
-     *                          Assumes control of this grid and deletes
-     *                          it when the class is destroyed.
-     * @param reflect_loss      Reflection loss model.  Defaults to a
-     *                          Rayleigh reflection for "sand" if NULL.
-     *                          The boundary_model takes over ownship of this
-     *                          reference and deletes it as part of its destructor.
-     */
-    boundary_grid_fast(data_grid_bathy* height,
-        reflect_loss_model* reflect_loss = NULL) :
-        boundary_model(reflect_loss), _height(height) {
-        this->_height->interp_type(0,GRID_INTERP_PCHIP);
-        this->_height->interp_type(1,GRID_INTERP_PCHIP);
-        this->_height->edge_limit(0,true);
-        this->_height->edge_limit(1,true);
-        if (_reflect_loss_model == NULL) {
-            _reflect_loss_model = new reflect_loss_rayleigh(
-                reflect_loss_rayleigh::SAND);
+            wvector1* normal = NULL, bool quick_interp = false) {
+        if (quick_interp) {
+            this->_height->interp_type(0, GRID_INTERP_LINEAR);
+            this->_height->interp_type(1, GRID_INTERP_LINEAR);
+        } else {
+            this->_height->interp_type(0, GRID_INTERP_PCHIP);
+            this->_height->interp_type(1, GRID_INTERP_PCHIP);
+        }
+        if (normal) {
+            double loc[2] = { location.theta(), location.phi() };
+            double grad[2];
+            *rho = this->_height->interpolate(loc, grad);
+            const double t = grad[0] / (*rho);      // slope = tan(angle)
+            const double p = grad[1] / ((*rho) * sin(location.theta()));
+            normal->theta(-t / sqrt(1.0 + t * t));  // normal = -sin(angle)
+            normal->phi(-p / sqrt(1.0 + p * p));
+            const double N = normal->theta() * normal->theta()
+                    + normal->phi() * normal->phi();
+            normal->rho(sqrt(1.0 - N));				// r=sqrt(1-t^2-p^2)
+        } else {
+            double loc[2] = { location.theta(), location.phi() };
+            *rho = this->_height->interpolate(loc);
         }
     }
 
-    /**
-     * Delete boundary grid.
-     */
-    virtual ~boundary_grid_fast() {
-        delete _height;
-    }
+protected:
 
-};
+    /** Boundary for all locations. */
+    data_grid_bathy* _height;
+
+}; // end class boundary_grid_fast
 
 }  // end of namespace ocean
 }  // end of namespace usml
--- ocean/profile_grid_fast.h
+++ ocean/profile_grid_fast.h
@@ -29,17 +29,33 @@ namespace ocean {
  *             to their spherical earth equivalents (altitude -> rho,
  *             theta,phi).
  */
-class profile_grid_fast : public profile_model
-{
-  protected:
+class profile_grid_fast : public profile_model {
+
+public:
 
     //**************************************************
     // sound speed model
 
-    /** Sound speed for all locations. */
-    data_grid_svp* _sound_speed ;
+    /**
+     * Constructor - Default behavior for new profile models.
+     *
+     * @param speed         Sound speed for the whole ocean (m/s).
+     *                      Assumes control of this grid and deletes
+     *                      it when the class is destroyed.
+     * @param attmodel      In-water attenuation model.  Defaults to Thorp.
+     *                      The profile_model takes over ownership of this
+     *                      reference and deletes it as part of its destructor.
+     */
+    profile_grid_fast(data_grid_svp* speed, attenuation_model* attmodel = NULL) :
+            profile_model(attmodel), _sound_speed(speed) {
+    }
 
-  public:
+    /**
+     * Destructor - Delete sound speed grid.
+     */
+    virtual ~profile_grid_fast() {
+        delete _sound_speed;
+    }
 
     /**
      * Compute the speed of sound and it's first derivatives at
@@ -49,56 +65,35 @@ class profile_grid_fast : public profile_model
      * @param speed         Speed of sound (m/s) at each location (output).
      * @param gradient      Sound speed gradient at each location (output).
      */
-    virtual void sound_speed( const wposition& location,
-        matrix<double>* speed, wvector* gradient=NULL )
-    {
-        if ( gradient ) {
-            matrix<double> rho( location.size1(), location.size2() ) ;
-            matrix<double> theta( location.size1(), location.size2() ) ;
-            matrix<double> phi( location.size1(), location.size2() ) ;
-            this->_sound_speed->interpolate(
-                location.rho(), location.theta(), location.phi(),
-                speed, &rho, &theta, &phi ) ;
-            gradient->rho(rho) ;
-            gradient->theta(theta) ;
-            gradient->phi(phi) ;
+    virtual void sound_speed(const wposition& location, matrix<double>* speed,
+            wvector* gradient = NULL) {
+        if (gradient) {
+            matrix<double> rho(location.size1(), location.size2());
+            matrix<double> theta(location.size1(), location.size2());
+            matrix<double> phi(location.size1(), location.size2());
+            this->_sound_speed->interpolate(location.rho(), location.theta(),
+                    location.phi(), speed, &rho, &theta, &phi);
+            gradient->rho(rho);
+            gradient->theta(theta);
+            gradient->phi(phi);
         } else {
-            this->_sound_speed->interpolate(
-                location.rho(), location.theta(), location.phi(),
-                speed ) ;
+            this->_sound_speed->interpolate(location.rho(), location.theta(),
+                    location.phi(), speed);
         }
-        this->adjust_speed( location, speed, gradient ) ;
+        this->adjust_speed(location, speed, gradient);
     }
 
-    //**************************************************
-    // initialization
+protected:
 
-    /**
-     * Default behavior for new profile models.
-     *
-     * @param speed         Sound speed for the whole ocean (m/s).
-     *                      Assumes control of this grid and deletes
-     *                      it when the class is destroyed.
-     * @param attmodel      In-water attenuation model.  Defaults to Thorp.
-     *                      The profile_model takes over ownership of this
-     *                      reference and deletes it as part of its destructor.
-     */
-    profile_grid_fast(
-        data_grid_svp* speed, attenuation_model* attmodel=NULL)
-        : profile_model(attmodel), _sound_speed(speed) { }
-
-    /**
-     * Delete sound speed grid.
-     */
-    virtual ~profile_grid_fast() {
-        delete _sound_speed ;
-    }
+    /** Sound speed for all locations. */
+    data_grid_svp* _sound_speed;
 
 };
+// end class profile_grid_fast
 
 /// @}
-}  // end of namespace ocean
-}  // end of namespace usml
+} // end of namespace ocean
+} // end of namespace usml
 
 #endif
 
