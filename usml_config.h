96ea009e53db1eaec81141ac2fa59d9f9b651283
Ted Burns <tburns@aegistg.com>
2014-01-09 15:11:40 -0500

    Updated files for WIN32 support.

--- usml_config.h
+++ usml_config.h
@@ -11,11 +11,11 @@
  * CMake automatically defines usml_EXPORT when compiling shared libraries.
 */
 #if defined(_MSC_VER) && defined(USML_DYN_LINK)
-    #ifdef usml_EXPORT
+    #ifdef usml_EXPORTS
         #define USML_DECLSPEC __declspec(dllexport)
-    #else
+	#else
         #define USML_DECLSPEC __declspec(dllimport)
-    #endif
+	#endif
 #else
     #define USML_DECLSPEC 
 #endif
