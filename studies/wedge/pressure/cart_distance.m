9a1c39951c137ca1b45284ad7b753a57c3613029
David Thibaudeau <dthibaudeau@aegistg.com>
2014-02-10 14:43:50 -0500

    Removed large files and incomplete test suite

--- studies/wedge/pressure/cart_distance.m
+++
@@ -1,9 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-%% Cartesian distance calculation
-
-function r = cart_distance(source_x, source_z,...
-			target_x, target_z)
-
-	r = sqrt( (source_x .- target_x).^2 + (source_z .- target_z).^2);
-
-end
