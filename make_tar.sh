b05cd046a5474497fa7fa902d6162b137104b776
David Thibaudeau <dthibaudeau@aegistg.com>
2014-03-24 14:08:14 -0400

    Corrected too shallow logic and updated make_tar
    Too shallow logic was supposed to turn the ray around when the ray was in too shallow of water.
    We need to remember this turn around logic twice, where previously it was only being considered
    once. We needed to check for too shallow once when we compute dot_full and secondly after we
    determine the exact location of the bottom interaction.
    
    Added new exclude files for the make_tar to bring the tar size within reasonable size.

--- make_tar.sh
+++ make_tar.sh
@@ -3,13 +3,15 @@ version=0.05b
 
 # Do not follow or archive symbolic links
 find * -type f | tar --exclude=data --exclude=doc --exclude=html --exclude=usml_api.html --exclude=make_tar* \
+	 --exclude '*.nc' --exclude '*.csv' --exclude '*.grd' --exclude '*.asc' \
     -czf ../usml-src-$version.tar.gz -T -
 tar -czf ../usml-doc-$version.tar.gz usml_api.html html/*
 tar -czf ../usml-data-$version.tar.gz data/*
 
 # Do not follow or archive symbolic links
 find * -type f | zip --quiet -r  ../usml-src-$version.zip -@ \
-    -x "data/*" -x "doc/*" -x "html/*" -x usml_api.html -x make_tar*
+    -x "data/*" -x "doc/*" -x "html/*" -x usml_api.html -x make_tar* -x '*.nc' -x '*.csv' \
+    -x '*.grd' -x '*.asc'
 zip --quiet -r ../usml-doc-$version.zip usml_api.html html/*
 zip --quiet -r ../usml-data-$version.zip data/*
 cp usml_install.html ..
