563823f6bb60e80f6c3f7d9631902a25afa99467
David Thibaudeau <dthibaudeau@aegistg.com>
2014-07-02 12:22:18 -0400

    Added commits and data used.

--- ocean/scattering_lambert.h
+++ ocean/scattering_lambert.h
@@ -17,10 +17,33 @@ using namespace usml::types ;
 
 using boost::numeric::ublas::vector;
 
-class USML_DECLSPEC scattering_lambert :
-        public scattering_model {
+/**
+ * Scattering model that follows Lambert's Law:
+ *
+ *\f[
+ * \mathcal{I}_{scat} \propto \mathcal{I}_{inc} sin(\alpha_I)
+ * sin(\alpha_S) \delta A
+ *\f]
+ */
+
+class USML_DECLSPEC scattering_lambert : public scattering_model {
 
     public:
+
+        /**
+         * Computes the broadband reflection loss and phase change.
+         *
+         * @param location      NOT USED
+         * @param frequencies   Frequencies over which to compute loss. (Hz)
+         * @param angleI        Depression incident angle (radians).
+         * @param angleS        Depression scattered angle (radians).
+         * @param azI           NOT USED
+         * @param azS           NOT USED.
+         * @param amplitude     Change in ray strength in dB (output).
+         * @param phase         NOT USED
+         *
+         * NOTE: All angles are relative to the scattering interface.
+         */
 
         virtual void scattering_strength( const wposition1& location,
             const seq_vector& frequencies, double angleI, double angleS,
