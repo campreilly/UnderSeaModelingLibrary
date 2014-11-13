a4bd7c9cfaf4be967fdfe1fd64840d0ecef17acd
David Thibaudeau <dthibaudeau@aegistg.com>
2014-11-13 14:56:41 -0500

    Changed all boost::math::isnan that are not present inside a debug statement to isnan, regarding issue #63. Added a small line in read_woa to test the functionality of boost::math::isnan, output is false in release and true in debug.

--- netcdf/test/read_profile_test.cc
+++ netcdf/test/read_profile_test.cc
@@ -115,6 +115,9 @@ BOOST_AUTO_TEST_CASE( read_woa ) {
         }
         ofile << std::endl ;
     }
+    cout << "does boost::math::isnan work? "
+         << ( ( (boost::math::isnan(NAN)) ) ? "yes" : "no" )
+         << endl ;
 }
 
 /**
