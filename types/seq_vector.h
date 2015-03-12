62f6b9c4c0e277d9031043b6a9b15c49e68ecc01
David Thibaudeau <dthibaudeau@aegistg.com>
2015-03-12 14:30:19 -0400

    Removed dependancy of ublas vector from seq_vector.

--- types/seq_vector.h
+++ types/seq_vector.h
@@ -20,16 +20,8 @@ using namespace usml::ublas;
  * Fast interpolation algorithms require an ability to quickly
  * lookup an axis index appropriate given a floating point axis value.
  *
- * This class caches data values and increments in ublas::vector<> objects.
- * This allow frequently used operations (ex: operator[] and increment())
- * access to inlined methods and cached values instead of computed results.
- * Sub-classes often store an additional representation of this data
- * that is used to implement fast versions of the find_index() method.
- *
- * This design uses zero_vector<T> as a model of how to implement a read-only
- * uBLAS vector.  Implements subclass methods as virtual functions,
- * instead of traits, so that they can be resolved at run-time.  Run-time
- * resolution is critical to reading sequences from netCDF data files.
+ * This class is modeled after ublas::vector and as such can be used
+ * in a similar fashion to other ublas::vectors.
  */
 class USML_DECLSPEC seq_vector: public vector_container<seq_vector>
 {
@@ -39,15 +31,13 @@ class USML_DECLSPEC seq_vector: public vector_container<seq_vector>
         typedef double  value_type ;
         typedef value_type  reference ;
         typedef const reference const_reference ;
-        typedef value_type* value_ptr ;
-        typedef vector<value_type>  vector_type ;
-        typedef vector_type::array_type array_type ;
-        typedef vector<value_type>::storage_category storage_category ;
+        typedef unbounded_array<value_type> array_type ;
         typedef array_type::size_type  size_type ;
+        typedef array_type::difference_type  difference_type ;
+        typedef dense_tag storage_category ;
         typedef indexed_const_iterator<self_type, dense_random_access_iterator_tag> const_iterator;
         typedef const_iterator  iterator ;
         typedef reverse_iterator_base<const_iterator> const_reverse_iterator;
-        typedef array_type::difference_type  difference_type ;
         typedef const vector_reference<const self_type> const_closure_type ;
         typedef vector_reference<self_type> closure_type ;
 
@@ -80,7 +70,7 @@ class USML_DECLSPEC seq_vector: public vector_container<seq_vector>
          * @param   index       The element number to retrieve (zero indexed).
          * @return              The value between the "index" element.
          */
-        reference operator[] ( size_type index ) const {
+        const_reference operator[] ( size_type index ) const {
             return _data[index] ;
         }
 
@@ -106,7 +96,7 @@ class USML_DECLSPEC seq_vector: public vector_container<seq_vector>
          * @return              The difference between the element at "index"
          *                      and the element at "index+1".
          */
-        reference increment( size_type index ) const {
+        const_reference increment( size_type index ) const {
             index = std::max( (size_type)0, std::min(_max_index,index) ) ;
             return _increment[index] ;
         }
