2fdb9ef50c4062e694e43e69f96674f904f4385e
Sean Reilly <sreilly@aegistg.com>
2015-03-25 10:12:21 -0400

    Integration testing for "define ocean characteristics" part of design. - Start building reverb_analytic_test.cc to role play "reverberation model". - Add reverb_analytic_test to usmlBuildStudies.cmake file. - Build the ocean for this scenario.  Use lock_ versions of models. - Update ocean_shared.

--- config/usmlBuildStudies.cmake
+++ config/usmlBuildStudies.cmake
@@ -16,23 +16,29 @@
     add_executable( pedersen_test studies/pedersen/pedersen_test.cc )
     target_link_libraries( pedersen_test usml )
     
-    add_executable( malta_movie studies/malta_movie/malta_movie.cc )
-    target_link_libraries( malta_movie usml )
-    
-    add_executable( malta_rays studies/malta_movie/malta_rays.cc )
-    target_link_libraries( malta_rays usml )
+#    add_executable( malta_movie studies/malta_movie/malta_movie.cc )
+#    target_link_libraries( malta_movie usml )
+#    
+#    add_executable( malta_rays studies/malta_movie/malta_rays.cc )
+#    target_link_libraries( malta_rays usml )
 
     add_executable( monostatic studies/reverberation/reverberation_monostatic.cc )
     target_link_libraries( monostatic usml )
-
+#
 #    add_executable( bistatic studies/reverberation/reverberation_bistatic.cc )
 #    target_link_libraries( bistatic usml )
+
+    add_executable( reverb_analytic_test studies/reverberation/reverb_analytic_test.cc )
+    target_link_libraries( reverb_analytic_test usml )
     
     set_property(
-       TARGET cmp_speed ray_speed eigenray_extra_test pedersen_test malta_movie malta_rays monostatic #bistatic
-       PROPERTY COMPILE_DEFINITIONS
+        TARGET cmp_speed ray_speed eigenray_extra_test pedersen_test 
+        # malta_movie malta_rays
+	monostatic #bistatic
+        reverb_analytic_test 
+        PROPERTY COMPILE_DEFINITIONS
         USML_DATA_DIR="${USML_DATA_DIR}"
         USML_STUDIES_DIR="${USML_STUDIES_DIR}"
-       )
+        )
 
 
--- studies/README.txt
+++ studies/README.txt
@@ -1,3 +1,4 @@
 The programs in these directories are tests that either take too long,
 or are too specialized, to be included as part of the standard 
-regression tests.  Each directory has its own Makefile for building it.
+regression tests.  These test are automatic built if UMSL_BUILD_STUDIES
+to set to true in CMake.
