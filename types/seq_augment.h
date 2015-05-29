c88aa25870bc93a9bff4bf3101dbf9e06c9cb428
Ted Burns <tburns@aegistg.com>
2015-05-29 11:02:53 -0500

    Back to no spacing.

--- types/seq_augment.h
+++ types/seq_augment.h
@@ -24,20 +24,17 @@ public:
     /**
      * Constructor
      * Takes an unaugmented seq_vector and adds a specified number of
-     * rays to the edges of the vector with the spacing provided.
-     * Only works on monotonically increasing seq_vectors. If 6 rays
+     * linearly spaced rays to the edges of the vector. ie, if 6 rays
      * are to be added to the seq_vector, 3 new rays will be created
-     * at the lower and upper extremes of the seq_vector by the spacing value.
+     * at the upper and lower extreme of the seq_vector.
      *
      * @param origin    seq_vector to augment
      * @param num_rays  number of rays to increase the vector by
-     * @param spacing   value to space the added rays by from the lower
-     *                  and upper extremes. Optional - Defaults to 0.1
      */
-    seq_augment( const seq_vector* origin, size_t num_rays, double spacing = 0.1  )
+    seq_augment( const seq_vector* origin, size_t num_rays )
         : seq_data( origin->size()+num_rays )
     {
-        initialize( origin, num_rays, spacing ) ;
+        initialize( origin, num_rays ) ;
     }
 
     /**
@@ -52,10 +49,8 @@ protected:
      *
      * @param origin    unagumented seq_vector
      * @param num_rays  number of rays to add to the seq_vector
-     * @param spacing   value to space the added rays by from the lower
-     *                  and upper extremes.
      */
-    void initialize( const seq_vector* origin, size_t num_rays, double spacing ) {
+    void initialize( const seq_vector* origin, size_t num_rays ) {
         size_t size_old( origin->size() ) ;
         size_t size( size_old + num_rays ) ;
         _index = 0 ;
@@ -67,22 +62,18 @@ protected:
         double first = (*origin)[0] ;
         _value = _index_data = first ;
         size_t half = floor(num_rays/2)+1 ;
-
+        double spacing = origin->increment(0) / half ;
         _data[0] = first ;
-        for(size_t i=1; i<half; ++i) {
+        for(size_t i=1; i<half+1; ++i) {
             _data[i] = _data[i-1] + spacing ;
         }
-
-		double* d = _data.begin()+half ;
+        double* d = _data.begin()+half+1 ;
         double* od = origin->data().begin() ;
-        memcpy(d, (od+1), sizeof(double)*(size_old-2) ) ;
-        
-        // set last value
-        _data[size-1] =  (*origin)[size_old-1];
-
-        for(size_t i=size-2; i > size-1-half; --i) {
-            _data[i] = _data[i+1] - spacing ;
+        memcpy(d, (od+2), sizeof(double)*(size_old-2) ) ;
+        for(size_t i=size_old-2+half; i<size-1; ++i) {
+            _data[i] = _data[i-1] + spacing ;
         }
+        _data[size-1] = (*origin)[size_old-1] ;
 
         for(size_t i=1; i<size; ++i) {
             _increment[i-1] = _data[i] - _data[i-1] ;
